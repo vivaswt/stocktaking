9546abd1dd459248e0465e49b35170b0e351ec41
vivaswt <vivaswt@gmail.com>
2022-12-22 20:33:42 +0900

    video test

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,26 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
 
+  const videoElement = React.useRef(null);
+  const [videoMsg, setVideoMsg] = React.useState('nope');
+
+  React.useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: {
+          facingMode: {exact: 'environment'}
+        }
+      }).then(stream => {
+        videoElement.current.srcObject = stream;
+        videoElement.current.onLoadedMetadata = () => {
+          videoElement.current.play();
+        };
+      }).catch(e => {
+        setVideoMsg(e.message);
+      });
+  }, []);
+
   const msg = "BarcodeDetector" in window ? 'supportted' : 'not supported';
 
   return (
@@ -17,6 +37,14 @@ function App() {
         <p>{!data ? "Loading..." : data}</p>
         <p>{msg}</p>
       </header>
+      <video
+        id="js-video"
+        class="reader-video"
+        autoplay
+        playsinline
+        ref={videoElement}
+      ></video>
+      <p>video message: {videoMsg}</p>
     </div>
   );
 }
