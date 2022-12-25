2e09e5b7dbb0706a1f51f9a4bcf701e5493aacda
vivaswt <vivaswt@gmail.com>
2022-12-25 11:52:21 +0900

    implement finding QR for test

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,88 @@
 import React from 'react';
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [windowState, setWindowState] = React.useState('result');
+  const [rawCode, setRawCode] = React.useState('no code');
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+  const beginScanning = () => {
+    setWindowState('scan');
+  };
 
+  if (windowState === 'result') {
+    return (
+      <ResultWindw rawCode={rawCode} beginScanning={beginScanning} />
+    );
+  }
+
+  const setResult = code => {
+    setRawCode(code);
+    setWindowState('result');
+  };
+
+  return (
+    <ScanWindow setResult={setResult} />
+  );
+}
+
+function ResultWindw({ rawCode, beginScanning }) {
+  return (
+    <div>
+      <div>raw code = {rawCode}</div>
+      <input type="button" value="scan code" onClick={beginScanning} />
+    </div>
+  );
+}
+
+function ScanWindow({ setResult }) {
   const videoElement = React.useRef(null);
   const [videoMsg, setVideoMsg] = React.useState('nope');
+
+  const findQR = (video, setResult) => {
+    const detector = new window.BarcodeDetector();
+    detector
+      .detect(video)
+      .then(barcodes => {
+        if (barcodes.length > 0) {
+          setResult(barcodes[0].rawValue);
+        } else {
+          setTimeout(() => findQR(video, setResult), 200);
+        }
+      }).catch(e => {
+        setResult(e.toString());
+      });
+  }
 
   React.useEffect(() => {
     navigator.mediaDevices
       .getUserMedia({
         audio: false,
         video: {
-          facingMode: {exact: 'environment'}
+          facingMode: { exact: 'environment' }
         }
       }).then(stream => {
         videoElement.current.srcObject = stream;
-        videoElement.current.onLoadedMetadata = () => {
+        videoElement.current.onloadedmetadata = () => {
           videoElement.current.play();
+          findQR(videoElement.current, setResult);
         };
       }).catch(e => {
+        console.log(e);
         setVideoMsg(e.message);
       });
   }, []);
 
-  const msg = "BarcodeDetector" in window ? 'supportted' : 'not supported';
-
   return (
     <div className="App">
-      <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
-        <p>{msg}</p>
-      </header>
       <video
         id="js-video"
-        class="reader-video"
-        autoplay
-        playsinline
+        className="reader-video"
+        autoPlay
+        playsInline
         ref={videoElement}
-      ></video>
+      />
+      <div>
+        <input type="button" value="Cancel" onClick={() => setResult('Canceled')} />
+      </div>
       <p>video message: {videoMsg}</p>
     </div>
   );
